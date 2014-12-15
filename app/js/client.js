49b79e9383864c7c7826935a95a6b13386c7495a
Scott Burbidge <scottisreally@gmail.com>
2014-12-14 19:39:31 -0800

    signIn, signUp added to user controller

---
+++ app/js/client.js
@@ -0,0 +1,6 @@
+require('angular/angular');
+require('angular-route');
+require('angular-cookies');
+require('angular-base64');
+
+var codeApp = angular.module('codeApp', ['ngRoute', 'ngCookies', 'base64']);
