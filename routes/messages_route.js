6c758abb4a7e69c037367385d70b77397d30b19c
Scott Burbidge <scottisreally@gmail.com>
2014-12-15 16:30:54 -0800

    eheh

---
+++ routes/messages_route.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Message = require('../model/message_model');
+var User = require('..model/user_model');
+
+module.exports = function(app, jwtauth) {
+
+  app.post('/api/message', jwtauth, function(req, res) {
+    User.findOne({'email': req.body.email});
+  });
+};
