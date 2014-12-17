456a85515ec300d9ec2b677ceca2bbe81bea6e84
Joe Elsey <joe.elsey@gmail.com>
2014-12-17 11:27:49 -0800

    user index controller

---
+++ app/js/users/controllers/user_index_controller.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(app) {
+  app.controller('userIndexController',['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location) {
+    if(!$cookies.jwt || !$cookies.jwt.length) return $location.path('/users');
+
+    $http.defaults.headers.common['jwt'] = $cookies.jwt;
+
+    $scope.allUsers = function() {
+      $http({
+        method: 'GET',
+        url: '/api/allusers'
+      })
+      .success(function(data) {
+        $scope.users = data;
+      })
+      .error(function(data) {
+        console.log(data);
+      });
+    };
+
+    $scope.User = function() {
+      $http({
+        method: 'GET',
+        url: '/api/user'
+      })
+      .success(function(data) {
+        $scope.user = data;
+      })
+      .error(function(data) {
+        console.log(data);
+      });
+    };
+
+    $scope.changeUserInfo = function() {
+      $http({
+        method: 'PUT',
+        url: '/api/user'
+        data: $scope.user
+      })
+      .success(function(data) {
+        $scope.user = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log(data);
+      });
+    };
+
+    $scope.deleteUser = function() {
+      $http({
+        method: ''
+      })
+    }
+  }])
+}
