238fc473061c685e3076d745eea9939c28ae00d7
Joe Elsey <joe.elsey@gmail.com>
2014-12-18 16:50:12 -0800

    changes

--- app/js/users/controllers/enrollment_controller.js
+++ app/js/users/controllers/enrollment_controller.js
@@ -1,7 +1,15 @@
 'use strict';
 
 module.exports = function() {
-  app.controller('enrollmentCtrl', ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location){
-    
+  app.controller('enrollmentCtrl', ['$scope', '$http', '$cookies', '$location', 
+    function($scope, $http, $cookies, $location){
+
+    if(!$cookies.jwt || !$cookies.jwt.length) return $location.path('/users');
+
+    $http.defaults.headers.common['jwt'] = $cookies.jwt;
+
+    $scope.addStudent = function() {
+      
+    }
   }])
 }
