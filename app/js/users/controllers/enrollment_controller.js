3453dd3eb63c97ce512b97702a21c69d705a620c
Joe Elsey <joe.elsey@gmail.com>
2014-12-18 16:37:56 -0800

    angular enrollment

---
+++ app/js/users/controllers/enrollment_controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function() {
+  app.controller('enrollmentCtrl', ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location){
+    
+  }])
+}
