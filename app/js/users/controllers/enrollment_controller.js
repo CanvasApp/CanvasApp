afbbbad1c14c11eaba88dba8ba03ce296ac1f956
Joe Elsey <joe.elsey@gmail.com>
2014-12-18 17:12:47 -0800

    clean up before the refactor

--- app/js/users/controllers/enrollment_controller.js
+++ app/js/users/controllers/enrollment_controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function(app) {
   app.controller('enrollmentCtrl', ['$scope', '$http', '$cookies', '$location', 
     function($scope, $http, $cookies, $location){
 
@@ -8,8 +8,52 @@ module.exports = function() {
 
     $http.defaults.headers.common['jwt'] = $cookies.jwt;
 
+    //for students only
     $scope.addStudent = function() {
-      
-    }
-  }])
-}
+      console.log('enroll student');
+      $http({
+        method: 'PUT',
+        url: '/api/studentenrollment/' + $scope.course.code
+      })
+      .success(function(data) {
+        console.log(data);
+        console.log($scope.course.code);
+      })
+      .error(function(data) {
+        console.log(data);
+      });
+    };
+
+    //for admins only
+    $scope.addTeacher = function() {
+      console.log('enroll teacher');
+      $http({
+        method: 'PUT',
+        url: '/api/teacherenrollment/' + $scope.course.code
+      })
+      .success(function(data) {
+        console.log(data);
+        console.log($scope.course.code);
+      })
+      .error(function(data) {
+        console.log(data);
+      });
+    };
+
+    //for teachers only
+    $scope.markPass = function() {
+      console.log('mark as pass');
+      $http({
+        method: 'PUT',
+        url: '/api/studentenrollmentpass/' + $scope.course.code
+      })
+      .success(function(data) {
+        console.log(data);
+        console.log($scope.course.code);
+      })
+      .error(function(data) {
+        console.log(data);
+      });
+    };
+  }]);
+};
